691b7a4e364a15b0f0908325cf268b6f79b6d88f
Andrey Mogilevskiy <andrey.mogilevskiy@hotmail.com>
2021-01-16 16:54:32 +0300

    Created a project using initializr.

---
+++ unit-spring-boot/src/main/java/otus/amogilevskiy/spring/UnitSpringBootApplication.java
@@ -0,0 +1,13 @@
+package otus.amogilevskiy.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitSpringBootApplication.class, args);
+	}
+
+}
